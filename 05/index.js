8432b892742da5ce32949902e5e514dff18467ac
Mushrrom <94596520+Mushrrom@users.noreply.github.com>
2022-12-10 17:05:41 +1100

    day 5 yayy

--- 05/index.js
+++ 05/index.js
@@ -1,3 +1,74 @@
 const fs = require('fs');
 
-let list = fs.readFileSync('input', 'utf8').split("\n")
+const list = fs.readFileSync('input', 'utf8').split("\n")
+function generateitems() {
+    let itemslist = [[], [], [], [], [], [], [], [], []]
+    for (let i = 7; i >= 0; i--) {
+        let items = list[i].replace(/[\[+\]]/g, " ")
+
+        for (let j = 0; j < items.length; j += 4) {
+            itemslist[j / 4].push(items[j + 1])
+        }
+
+    }
+
+    for (let i = 0; i < itemslist.length; i++) {
+        for (let j = 0; j < itemslist[i].length; j++) {
+            if (itemslist[i][j] === ' ') {
+                itemslist[i].splice(j, 100)
+                break
+            }
+        }
+
+    }
+    return itemslist
+}
+
+itemslist = generateitems()
+
+for (let i = 9; i < list.length-1; i++) {
+    let instructions = list[i].replace(/move |from |to /g, "")
+    instructions = instructions.split(" ")
+    let movefrom = instructions[1]-1
+    let moveto = instructions[2]-1
+    for (let j = 0; j < parseInt(instructions[0]); j++){
+
+        let letter = itemslist[movefrom].at(-1)
+        itemslist[movefrom].splice(-1, 1)
+        itemslist[moveto].push(letter)
+
+    }
+}
+let final = ""
+for (let i = 0; i < itemslist.length; i++){
+    final += itemslist[i].at(-1)
+}
+
+console.log(final)
+
+itemslist = generateitems()
+for (let i = 9; i < list.length -1; i++) {
+    let instructions = list[i].replace(/move |from |to /g, "")
+    instructions = instructions.split(" ")
+    let movefrom = instructions[1]-1
+    let moveto = instructions[2]-1
+    let tomove = []
+    for (let j = 0; j < parseInt(instructions[0]); j++) {
+
+        let letter = itemslist[movefrom].at(-1)
+        tomove.unshift(letter)
+        itemslist[movefrom].splice(-1, 1)
+
+    }
+    for (let j = 0; j < tomove.length; j++){
+        itemslist[moveto].push(tomove[j])
+    }
+
+
+}
+final = ""
+for (let i = 0; i < itemslist.length; i++){
+    final += itemslist[i].at(-1)
+}
+
+console.log(final)
--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-if ( 2===2 && 2===2 ) {console.log("a")}
+list = [1, 2, 3, 4, 5]
+
+console.log(list.at(-1))
+
+list.splice(-1, 1)
+list.push(["a", "b"])
+console.log(list)
+
+console.log(list[list.length-1])
